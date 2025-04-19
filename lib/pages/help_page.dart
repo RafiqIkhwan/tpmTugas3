1170f470815790e2f9053f473bdc444c03a722d4
unknown <rafiqikhwan16@gmail.com>
2025-04-19 17:06:04 +0700

    update help page

--- lib/pages/help_page.dart
+++ lib/pages/help_page.dart
@@ -10,59 +10,60 @@ class HelpPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bantuan & Logout'),
+        title: const Text('Bantuan & Logout'),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pushReplacement(
               context,
               MaterialPageRoute(
-                builder: (context) => HomePage(),
+                builder: (context) => const HomePage(),
               ), // Kembali ke MainPage
             );
           },
         ),
       ),
-      body: Padding(
+      body: SingleChildScrollView(
+        // Tambahkan SingleChildScrollView
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
+            const Text(
               'Panduan Penggunaan Aplikasi',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 20),
-            Text(
+            const SizedBox(height: 20),
+            const Text(
               '1. Stopwatch: Fitur ini digunakan untuk mengukur waktu secara presisi. Anda dapat memulai, menghentikan, dan mereset stopwatch sesuai kebutuhan.',
               style: TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               '2. Jenis Bilangan: Fitur ini membantu Anda menentukan jenis bilangan, seperti bilangan prima, desimal, bulat positif/negatif, atau bilangan cacah, berdasarkan input yang diberikan.',
               style: TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               '3. Tracking LBS: Fitur ini memungkinkan Anda untuk melacak lokasi menggunakan layanan berbasis lokasi (LBS). Lokasi Anda akan ditampilkan dalam bentuk koordinat latitude dan longitude.',
               style: TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               '4. Konversi Waktu: Fitur ini digunakan untuk mengonversi waktu dari satu satuan ke satuan lainnya, seperti tahun ke jam, menit, atau detik.',
               style: TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               '5. Daftar Situs Rekomendasi: Fitur ini menyediakan daftar situs rekomendasi yang dilengkapi dengan gambar dan link yang dapat Anda kunjungi.',
               style: TextStyle(fontSize: 16),
             ),
-            SizedBox(height: 10),
-            Text(
+            const SizedBox(height: 10),
+            const Text(
               '6. Favorit: Fitur ini memungkinkan Anda untuk menandai situs favorit dari daftar rekomendasi, sehingga Anda dapat mengaksesnya dengan lebih mudah.',
               style: TextStyle(fontSize: 16),
             ),
-            Spacer(),
+            const SizedBox(height: 20),
             Center(
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -72,10 +73,10 @@ class HelpPage extends StatelessWidget {
                 onPressed: () {
                   Navigator.pushReplacement(
                     context,
-                    MaterialPageRoute(builder: (context) => LoginPage()),
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
                   );
                 },
-                child: Text('Logout'),
+                child: const Text('Logout'),
               ),
             ),
           ],
